8c9f133bdd8f7fafb9bc485ff668a5033fe43bef
TetRex <tetrex228@pm.me>
2026-03-28 21:19:01 +0200

    Refactor client.py to improve sender and receiver logic; add keypair_generate.py for key generation functions

--- client.py
+++ client.py
@@ -1,24 +1,19 @@
-a = int(input("Are reciever(1) or sender(2): "))
-p = 456
-q = 324
-e = 40
-N = p * q
-fN = (p - 1) * (q - 1)
-public_key = {fN,e}
-print(public_key)
-"""
-def sender():
-    print("You are sender")
-    massege = input()
-    public_key = input("Input recievers public key: ")
-    
-if a == 1:
-    print("You are reciever")
-    print(f"Your public key: {public_key}")
-elif a == 2:
-    sender()
-else:
-    raise Exception("Wrong answer")
+from keypair_generate import generate_keypair
 
+a = int(input("Are you sender(1) or reciever(2): "))
 
-"""
+keypair = generate_keypair()
+
+def massage_convert(massage):
+    return massage == hash(message)
+
+match a:
+    case 1:
+        print("You are sender")
+        public_key = input("Insert reciever public_key")
+        def encrypt(message, public_key):
+            n, e = public_key
+            encrypted_message = [pow(ord(char), e, n) for char in message]
+            return encrypted_message
+    case 2:
+        print("You are reciever")
